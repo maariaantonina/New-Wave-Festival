e45ea1175aa829d9170ec9cd6013df61c1b2e1ac
Maria Kasińska <mariakasinska17@gmail.com>
2022-01-06 23:50:03 +0100

    trying to fix heroku problem

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 //middleware
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
